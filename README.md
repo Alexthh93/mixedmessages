1a33ef0c4fd8434ba8e2e0e5029f4402820aa496
Bam <andrefmartins92@hotmail.com>
2021-10-21 15:35:31 +0100

    Initial commit with game plan

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mixed Messages - Codecademy Programming Portfolio
+
+This script will select random elements from different arrays and pair them together to make a cohesive sentence
---
+++ mixedmessages.js
@@ -0,0 +1,24 @@
+/* Mixed Messages Game Plan
+
+ create a list of topics - (Sports, Video-gaming, traveling, finance, animals)
+
+ Create 3 arrays which defines start, middle, end. (5 inputs in each array of each topic)
+
+ introductory list - array
+
+ middle list - array
+
+/ending list - array
+
+function to loop through the arrays and randomly select an entry. 
+
+ store each sentence into a variable and make sure to not replicate the same sentences. 
+
+ after storing the result, create a function that prints X amount of sentences
+
+ create a do...while loop that will go through the words array UNTIL they're different. /*
+
+ /* if end === end
+      it goes through the loop again until the results are different
+    replicate this n (number) of times. */
+    
